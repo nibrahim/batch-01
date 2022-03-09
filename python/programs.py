293ff65cc962c19b738970668b5a8a32ea87840b
Noufal Ibrahim <noufal@nibrahim.net.in>
2022-03-09 17:17:32 +0530

    Adds python files

---
+++ python/programs.py
@@ -0,0 +1,17 @@
+import string
+
+def freq(s):
+    d = {}
+    for i in s:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    return d
+
+
+def panagram(s):
+    for i in string.ascii_lowercase:
+        if i not in s:
+            return False
+    return True
---
+++ python/test_programs.py
@@ -0,0 +1,19 @@
+from programs import freq, panagram
+
+def test_same():
+    ret = freq("aaa")
+    assert ret == {"a" : 3}
+
+def test_multiple():
+    ret = freq("aaabbcccc")
+    assert ret == {"a": 3, "b" : 2, "c": 4}
+
+def test_panagram_true():
+    assert panagram("the quick brown fox jumps over the lazy dog")
+
+def test_panagram_false():
+    assert not panagram("the quick brown fox jumped over the lazy dog")
+
+
+
+
