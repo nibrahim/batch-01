9e6b77530aa7c066f1c0663b2b81e6478d681ff5
Noufal Ibrahim <noufal@nibrahim.net.in>
2022-03-10 16:38:26 +0530

    RED : Adds failing test for number of characters

---
+++ ari/ari.py
@@ -0,0 +1,7 @@
+"""
+Utilities to calculate automated readability index as per forumula in https://en.wikipedia.org/wiki/Automated_readability_index
+
+Copyright 2021 - Noufal Ibrahim <noufal@hamon.in>
+"""
+
+
---
+++ ari/test_ari.py
@@ -0,0 +1,6 @@
+import ari
+
+def test_nchars_0():
+    assert ari.nchars("") == 0
+    
+    
