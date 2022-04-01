6ca7ec7722144f735e8a928aa545a4b5749dfaa2
Noufal Ibrahim <noufal@nibrahim.net.in>
2022-04-01 16:48:10 +0530

    Adds artist selection

--- eg1.py
+++ eg1.py
@@ -5,14 +5,18 @@ app = Flask(__name__)
 
 import lyrics_test
 
-@app.route("/lyrics")
-def list_all_songs(artist="A"):
-    songs = lyrics_test.get_all_songs(artist)
-    return render_template("songlist.html", artist=artist, songs=songs)
+@app.route("/lyrics/<int:aid>")
+def list_all_songs(aid):
+    songs = lyrics_test.get_all_songs(aid)
+    return render_template("songlist.html", 
+                           artist="", 
+                           songs=songs)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    artists = lyrics_test.get_all_artists()
+    return render_template("index.html", artists=artists)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
